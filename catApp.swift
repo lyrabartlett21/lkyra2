af61f2f875640f7fcff9d0f5b6a688c15fb50467
lyrabartlett21 <96067222+lyrabartlett21@users.noreply.github.com>
2021-12-13 14:52:34 +0000

    Add files via upload

---
+++ catApp.swift
@@ -0,0 +1,17 @@
+//
+//  catApp.swift
+//  cat
+//
+//  Created by Jacob on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct catApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
